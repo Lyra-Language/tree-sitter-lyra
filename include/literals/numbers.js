9de0a5c81648137490bd89a12deeb3b33b3b1b4f
Avram Eisner <avrame@gmail.com>
2026-04-22 16:05:45 -0700

    Refactor number literal handling in grammar: Updated the grammar and parser to introduce named prefixes for binary, octal, and hexadecimal integer literals, enhancing clarity and maintainability. Adjusted related node types and tests to reflect these changes, ensuring consistent handling of number literals across the language.

--- include/literals/numbers.js
+++ include/literals/numbers.js
@@ -1,15 +1,26 @@
 const { PREC } = require("../prec");
 
 module.exports = {
-  _number_literal: $ => choice($.integer_literal, $.float_literal),
+  _number_literal: ($) => choice($.integer_literal, $.float_literal),
 
-  integer_literal: $ => choice($.decimal_int, $.octal_int, $.hexadecimal_int, $.binary_int),
-  
-  
-  decimal_int: $ => prec(PREC.DECIMAL_INT_TOKEN, token(/[0-9_]+/)),
-  binary_int: $ => seq('0b', token.immediate(/[01_]+/)),
-  octal_int: $ => seq('0o', token.immediate(/[0-7_]+/)),
-  hexadecimal_int: $ => seq('0x', token.immediate(/[0-9a-fA-F_]+/)),
-  
-  float_literal: $ => prec(PREC.FLOAT_LITERAL_TOKEN, token(/[0-9_]+\.[0-9_]+([eE][+-]?[0-9_]+)?/)),
-}
+  integer_literal: ($) =>
+    choice($.decimal_int, $.octal_int, $.hexadecimal_int, $.binary_int),
+
+  decimal_int: ($) => prec(PREC.DECIMAL_INT_TOKEN, token(/[0-9_]+/)),
+
+  binary_int: ($) => seq($.binary_prefix, token.immediate(/[01_]+/)),
+  binary_prefix: ($) => "0b",
+
+  octal_int: ($) => seq($.octal_prefix, token.immediate(/[0-7_]+/)),
+  octal_prefix: ($) => "0o",
+
+  hexadecimal_int: ($) =>
+    seq($.hexadecimal_prefix, token.immediate(/[0-9a-fA-F_]+/)),
+  hexadecimal_prefix: ($) => "0x",
+
+  float_literal: ($) =>
+    prec(
+      PREC.FLOAT_LITERAL_TOKEN,
+      token(/[0-9_]+\.[0-9_]+([eE][+-]?[0-9_]+)?/),
+    ),
+};
